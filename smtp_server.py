976438b1562c34b3b01ca1fc55fdd98024ee5e44
Abhimanyu Yadav <abhimanyu1992002@gmail.com>
2025-05-25 10:33:28 +0530

    Refactor SMTP server into a class-based structure

--- smtp_server.py
+++ smtp_server.py
@@ -1,41 +1,61 @@
 import socket
 import logging
-import time
+import sys
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # creating a tcp socket
 
-port = 25
+class SMTPServer():
 
-s.bind(("",port))
-logging.info(f"Binding my smtp server with port {port}")
+    def __init__(self, port = 25):
+        self.port = port
+        self.s = None
 
-s.listen(128)
-logging.info("SMTP server is on listening mode right now")
+    def start_smtp_server(self):
+        self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # creating a tcp socket
+        self.s.bind(("",self.port))
+        logging.info(f"Binding my smtp server with port {self.port}")
+
+        self.s.listen(128)
+        logging.info("SMTP server is on listening mode right now")
+
+        self._start_accepting_connections()
+
+    def _start_accepting_connections(self):
+        smtp_server = self.s
+
+        if not smtp_server:
+            logging.error("SMTP server is not started properly yet, please restart it again")
+            sys.exit()
+
+        while True:
+            conn, addr = smtp_server.accept()
+            logging.info(f"Got a connection from the address : {addr}")
+            conn.send("220 PROJECT SMTP SERVER IS READY..!!\r\n".encode()) # opening message for the client connection
+
+            self._handling_user_commonds(conn)
+
+    def _handling_user_commonds(self,conn):
+        # handling command
+        encoded_user_commond = b""
+
+        while not encoded_user_commond.endswith(b"\r\n"):
+            partial_commond = conn.recv(1024)
+            encoded_user_commond += partial_commond
+
+        user_commond = encoded_user_commond.decode().strip()
+        logging.info(f"Got an user command {user_commond}")
+
+        user_commond_type = user_commond.split(" ")[0]
+
+        if user_commond_type == "HELO":
+            self._handling_helo_commond(user_commond,conn)
 
 
-# currently our smtp server, only handles one connection at a time
-# maybe in future we could add multiple threads for accepting connection
-while True:
-    conn, addr = s.accept()
-    logging.info(f"Got a connection from the address : {addr}")
-    conn.send("220 PROJECT SMTP SERVER IS READY..!!\r\n".encode()) # opening message for the client
-
-    # handling command
-    encoded_user_commond = b""
-
-    while not encoded_user_commond.endswith(b"\r\n"):
-        partial_commond = conn.recv(1024)
-        encoded_user_commond += partial_commond
-
-    user_commond = encoded_user_commond.decode().strip()
-    logging.info(f"Got an user command {user_commond}")
-
-    time.sleep(3)
-
-    user_commond_type = user_commond.split(" ")[0]
-
-    if user_commond_type == "HELO":
+    def _handling_helo_commond(self,user_commond, conn):
         user_commond_value = user_commond.split(" ")[1].strip()
         conn.send(f"Hey {user_commond_value},pleased to meet you\r\n".encode())
         logging.info("Successfully answered the HELO command")
+
+if __name__ == "__main__":
+    server = SMTPServer()
+    server.start_smtp_server()
