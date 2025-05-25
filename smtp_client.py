3db2f577cb9b12277279ddbb9ffb0bf5ebe4d920
Abhimanyu Yadav <abhimanyu1992002@gmail.com>
2025-05-25 11:10:07 +0530

    add interactive commond support on client

--- smtp_client.py
+++ smtp_client.py
@@ -1,29 +1,54 @@
 import socket
 import logging
+import sys
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 s = socket.socket()
 
-port = 25 # i will connect to this port
+class SmtpClient():
+    def __init__(self,port = 25) -> None:
+        self.port = port
 
-s.connect(("127.0.0.1",port))
+    def connect(self,server_ip="127.0.0.1"):
+        s.connect((server_ip, self.port))
+        logging.info(f"Connected to smtp server at port {self.port}")
 
-# initial data from the smtp server
-initial_data = b""
+        # Getting initial data from the smtp server
+        self._handling_initial_data_from_server()
+        self._accept_commonds()
 
-while not initial_data.endswith(b"\r\n"):
-    initial_data = s.recv(1024)
+    def _handling_initial_data_from_server(self):
+        initial_data = b""
 
-logging.info(initial_data.decode())
+        while not initial_data.endswith(b"\r\n"):
+            initial_data = s.recv(1024)
 
-logging.info("Sending the HELO command")
-s.send("HELO abhimanyu.dev\r\n".encode())
+        logging.info(initial_data.decode())
 
-logging.info("Receiving the HELO command response")
-helo_response= b""
-while not helo_response.endswith(b"\r\n"):
-    part_helo_response = s.recv(1024)
-    helo_response += part_helo_response
-logging.info(helo_response.decode())
 
-s.close()
+    def _accept_commonds(self):
+        print("\nNow you can communicate with the server using smtp commonds, close connection using :q\n")
+        while True:
+            commond = input()
+            if commond == ":q":
+                s.close()
+                sys.exit()
+            else:
+                self._sending_commonds(commond)
+
+    def _sending_commonds(self,commond):
+        s.send(f"{commond}\r\n".encode())
+        logging.info("Commond send..!!")
+        self._handling_commond_response()
+
+    def _handling_commond_response(self):
+        response= b""
+        while not response.endswith(b"\r\n"):
+            part_response = s.recv(1024)
+            response += part_response
+        logging.info(response.decode())
+
+
+if __name__ == "__main__":
+    server = SmtpClient()
+    server.connect()
--- smtp_server.py
+++ smtp_server.py
@@ -5,7 +5,6 @@ import sys
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 
 class SMTPServer():
-
     def __init__(self, port = 25):
         self.port = port
         self.s = None
@@ -20,6 +19,7 @@ class SMTPServer():
 
         self._start_accepting_connections()
 
+    # Here we are accepting the commonds on a single thread, will add concurreny to it
     def _start_accepting_connections(self):
         smtp_server = self.s
 
@@ -30,12 +30,15 @@ class SMTPServer():
         while True:
             conn, addr = smtp_server.accept()
             logging.info(f"Got a connection from the address : {addr}")
-            conn.send("220 PROJECT SMTP SERVER IS READY..!!\r\n".encode()) # opening message for the client connection
 
-            self._handling_user_commonds(conn)
+            # opening message for the client connection
+            conn.send("220 PROJECT SMTP SERVER IS READY..!!\r\n".encode())
+
+            while True:
+                self._handling_user_commonds(conn)
+
 
     def _handling_user_commonds(self,conn):
-        # handling command
         encoded_user_commond = b""
 
         while not encoded_user_commond.endswith(b"\r\n"):
@@ -43,18 +46,22 @@ class SMTPServer():
             encoded_user_commond += partial_commond
 
         user_commond = encoded_user_commond.decode().strip()
-        logging.info(f"Got an user command {user_commond}")
+        logging.info(f"\nGot an user command `{user_commond}`")
 
         user_commond_type = user_commond.split(" ")[0]
 
         if user_commond_type == "HELO":
             self._handling_helo_commond(user_commond,conn)
+        else:
+            conn.send("502 Command not implemented\r\n".encode())
+            logging.warning(f"Unknown command: {user_commond}")
 
 
     def _handling_helo_commond(self,user_commond, conn):
         user_commond_value = user_commond.split(" ")[1].strip()
         conn.send(f"Hey {user_commond_value},pleased to meet you\r\n".encode())
-        logging.info("Successfully answered the HELO command")
+        logging.info("Successfully answered the HELO command\n")
+
 
 if __name__ == "__main__":
     server = SMTPServer()
